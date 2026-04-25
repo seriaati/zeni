b5230b48143759f1555bd65dac98257f6a45edb1
seria <seria.ati@gmail.com>
2026-04-26 00:44:58 +0900

    fix(pwa): disable PWA dev options

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
         navigateFallbackDenylist: [/^\/api\//],
       },
       devOptions: {
-        enabled: true,
+        enabled: false,
       },
     }),
   ],
